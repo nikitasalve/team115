19b1c579f14195f46b1842c8c6b12b5014ea82c5
Omkar <omkarW@hexaware.com>
2019-02-01 17:14:15 +0530

    myproject

---
+++ leavemanager/src/main/java/com/hexaware/FTP115/model/LeaveStatus.java
@@ -0,0 +1,21 @@
+package com.hexaware.FTP115.model;
+
+/**
+ * LeaveStatus enum to store LeaveStatus data.
+ * @author hexaware
+ */
+
+public enum LeaveStatus {
+  /**
+  * leaveType to store EL.
+  */
+  APPROVED,
+    /**
+    * leaveType to store PL.
+    */
+  DENIED,
+    /**
+    * leaveType to store ML.
+    */
+  PENDING;
+}
